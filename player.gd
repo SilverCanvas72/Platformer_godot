525d0c794300de9b944c638ef6861897e92803e7
SilverCanvas72 <idewij27@igs.vic.edu.au>
2024-06-27 20:21:52 +1000

    Player jumping / movement system (almost)
    Tried to make player jumping system but there is currently a bug

---
+++ player.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+const fall: int = 1000
+const max_fall: int = 600
+const jump_height: int = -500
+var flying: bool = false
+var falling: bool = false
+const start_point = Vector2(100, 450)
+
+
+func reset():
+	falling = false
+	flying=false
+	position = start_point
+
+
+func _ready():
+	reset()
+	
+	
+func _physics_process(delta):
+	if flying == true or falling == true:
+		velocity.y += fall + delta
+		
+	if velocity.y > max_fall:
+		velocity.y = max_fall
+		
+		
+func fly():
+	velocity.y = jump_height
