f14c97fdced6967d5ef8c2fddfb2e1b73e0e16ba
SilverCanvas72 <idewij27@igs.vic.edu.au>
2024-06-27 21:25:53 +1000

    "debugging"
    Frantic debbuging likely cause even more bugs.

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -5,11 +5,10 @@ var game_over : bool = false
 var score: int = 0
 var scroll: int = 0
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	newgame()
-	
+
 func newgame():
 	game_running = false
 	game_over = false
@@ -17,17 +16,20 @@ func newgame():
 	scroll = 0
 	$Player.reset()
 	
+
+	
+
 func _input(event):
 	if game_over == false:
-		if event.is_action_pressed("jump"):
-			if game_running == false:
-				start_game()
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+				if game_running == false:
+					start_game()
 			else:
-				if $Player.flying:
-					$Player.jump()
-						
+				if $Player.flying == true:
+					$Player.fly()
+
 func start_game():
 	game_running = true
 	$Player.flying = true
-	$Player.jump()			
-	
+	$Player.fly()
--- player.gd
+++ player.gd
@@ -5,22 +5,25 @@ const max_fall: int = 600
 const jump_height: int = -500
 var flying: bool = false
 var falling: bool = false
-const start_point = Vector2(100, 450)
+const start_point = Vector2(100, 400)
 
 
-func reset():
-	falling = false
-	flying=false
-	position = start_point
 
 
 func _ready():
 	reset()
+
+func reset():
+	falling = false
+	flying = false
+	position = start_point
+
+	
 	
 	
 func _physics_process(delta):
 	if flying == true or falling == true:
-		velocity.y += fall + delta
+		velocity.y += fall * delta
 		
 	if velocity.y > max_fall:
 		velocity.y = max_fall
