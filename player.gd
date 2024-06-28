045d7a50050e9d933fdc55ae32464b7ae7b270ec
SilverCanvas72 <idewij27@igs.vic.edu.au>
2024-06-29 09:21:10 +1000

    Debugged and made jumping system different
    Fixed jumping system and worked with physics to make it seem smooth

--- player.gd
+++ player.gd
@@ -1,33 +1,37 @@
 extends CharacterBody2D
 
-const fall: int = 1000
-const max_fall: int = 600
+const fall: int = 10
 const jump_height: int = -500
-var flying: bool = false
-var falling: bool = false
-const start_point = Vector2(100, 400)
+var started : bool = false
 
 
 
-
-func _ready():
-	reset()
-
-func reset():
-	falling = false
-	flying = false
-	position = start_point
-
-	
-	
-	
 func _physics_process(delta):
-	if flying == true or falling == true:
-		velocity.y += fall * delta
+	
+	velocity.y += fall
+	
+	if Input.is_action_just_pressed("Jump"):
+		if velocity.y > -5:
+			velocity.y += -400
+			
+		else :
+			velocity.y = -400
+	move_and_slide()
 		
-	if velocity.y > max_fall:
-		velocity.y = max_fall
+	
+	
+	
+
+
+
+
+
+	#if started == true:
+			#velocity.y += fall
 		
-		
-func fly():
-	velocity.y = jump_height
+	
+	#if Input.is_action_just_pressed("Jump"):
+		#velocity.y += jump_height
+		#started == true
+	#move_and_slide()
+
